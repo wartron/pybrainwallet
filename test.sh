805f75379ad03a1c333543d515ec18bdb11dcf1e
bitdev <bitdev@w-ll.org>
2013-02-20 23:09:07 -0800

    working - added test.sh

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "('1CeU9ugjwfsnzrhqjKy1HUBzXCCXVC76m1', '5JhmqDih2bTprJLZJYwUgnsAiHNbU8DReuBFMKCq5nPAmj1PeYQ')"
+echo "These should match!"
+./brainwallet.py "Man made it to the moon,, and decided it stinked like yellow cheeeese."
